c0833a8caf7fc330f1db2475d98f8e18e239ab61
Farah <fmz10@mail.aub.edu>
2022-12-15 08:41:12 +0200

    final touches

--- CMPS278Project/Models/Login.cs
+++ CMPS278Project/Models/Login.cs
@@ -5,10 +5,10 @@ namespace Login.Models
     public class LoginUser
     {
         [Required(ErrorMessage = "Please enter your name.")]
-        public string UserName { get; set; } = null!;
+        public string? UserName { get; set; }
         
         [Required(ErrorMessage = "Please enter a password.")]
         [DataType(DataType.Password)]
-        public string Password { get; set; } = null!;
+        public string? Password { get; set; }
     }
 }
--- CMPS278Project/Models/Register.cs
+++ CMPS278Project/Models/Register.cs
@@ -5,19 +5,19 @@ namespace Register.Models
     public class RegisterUser
     {
         [Required(ErrorMessage = "Please enter a username.")]
-        public string UserName { get; set; } = null!;
+        public string? UserName { get; set; }
         
         [Required(ErrorMessage = "Please enter a password.")]
         [DataType(DataType.Password)]
-        public string Password { get; set; } = null!;
+        public string? Password { get; set; }
 
         [Required]
-        public string Email { get; set; } = null!;
+        public string? Email { get; set; }
 
         [Required(ErrorMessage = "Please enter an address.")]
-        public string Address { get; set; } = null!;
+        public string? Address { get; set; }
 
         [Required(ErrorMessage = "Please your phone number.")]
-        public string PhoneNumber { get; set; } = null!;
+        public string? PhoneNumber { get; set; }
     }
 }
